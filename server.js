cf2535dec4de99fed0a2e50e4aa499871387f3af
HannahYiCodes <hannahyi.student@careerdevs.com>
2022-09-08 17:50:06 -0400

    Front End Updates

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const app = express()
 
 //allows frontend to server static files
 app.use(express.static('public'))
+app.use(express.json())
 
 //create a route for the root path
-app.get('/test', function (req, res) {
-    res.send('Hello World')
-}) 
+// app.get('/test', function (req, res) {
+//     console.log(req.body);
+//     res.send('Hello World')
+// }) 
 
 //sets the port to listen on
-app.listen(3500, () => {
-    console.log('Server listening on port 3500')
+app.listen(4000, () => {
+    console.log('Server listening on port 4000')
 })
